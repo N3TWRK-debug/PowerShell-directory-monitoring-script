dc5e39f5667cee4584504401e8cb878b55a39a87
N3TWRK-debug <Marsellus.Wallace1994@outlook.de>
2024-09-30 21:16:03 +0200

    Add files via upload

---
+++ Directory monitoring.ps1
@@ -0,0 +1,23 @@
+﻿Add-Type -AssemblyName PresentationCore, PresentationFramework
+
+# Set the target directory
+$directory = ""
+
+# Start monitoring
+$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.Path = $directory
+$watcher.Filter = "*.exe" # Use filter for specific file types
+$watcher.IncludeSubdirectories = $false
+$watcher.EnableRaisingEvents = $true
+
+# Define event handler
+Register-ObjectEvent $watcher "Created" -Action {
+    $message = "A new file has appeared in the directory" 
+    $wsh = New-Object -ComObject Wscript.Shell
+    $wsh.Popup($message, 0, "New File Found", 64)
+}
+
+# Start an infinite loop to keep the script running
+while ($true) {
+    Start-Sleep -Seconds 1
+}
